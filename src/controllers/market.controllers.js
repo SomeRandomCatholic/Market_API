48b6e477bf55275dc116822a2745921218775a32
Christopher Cruz <102389314+SomeRandomCatholic@users.noreply.github.com>
2024-10-01 12:45:13 -0600

    Update market.controllers.js

--- src/controllers/market.controllers.js
+++ src/controllers/market.controllers.js
@@ -65,7 +65,7 @@ export const getUsuario = async (req, res) => {
       }
       res.json({ message: "Producto Agregado" });
     } catch (error) {
-        console.log($cantidad);
+        console.log(cantidad);
       return res.status(500).json({ message: `Algo salio mal ${cantidad} y ${dateHoy}`});
     }
   };
